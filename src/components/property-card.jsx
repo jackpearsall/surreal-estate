d3b0036e1002f4e0ac3b3e68eb8e08ca587d8ee7
Jack Pearsall <jack.pearsall@visiosound.co.uk>
2019-05-02 16:49:51 +0100

    Created icons and styling property card

--- src/components/property-card.jsx
+++ src/components/property-card.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { number, string } from 'prop-types';
 import '../styles/property-card.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPoundSign, faBath, faBed, faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
+
+library.add(faPoundSign, faBath, faBed, faEnvelope);
 
 const Property = props => (
   <div className="property-card">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-type">{props.type}</p>
+    <h3 className="card-title">{props.title}</h3>
+    <p className="card-type-city">{props.type} - {props.city}</p>
     <ul className="card-details">
-      <li className="card-price">£{props.price}</li>
-      <li className="card-beds">{props.bedrooms} bedroom(s)</li>
-      <li className="card-baths">{props.bathrooms} bathroom(s)</li>
-      <li className="card-city">{props.city}</li>
-      <li className="card-email">{props.email}</li>
+      <li className="card-beds"><div className="icon"> <FontAwesomeIcon icon="bed" /></div>{props.bedrooms}</li>
+      <li className="card-baths"><div className="icon"> <FontAwesomeIcon icon="bath" /></div>{props.bathrooms}</li>
+      <li className="card-price"><div className="icon"> <FontAwesomeIcon icon="pound-sign" /></div>{props.price}</li>
+      <Link className="card-email" to="/"><div className="icon"> <FontAwesomeIcon icon="envelope" /></div>{props.email}</Link>
     </ul>
   </div>
 );
--- src/components/propeties.jsx
+++ src/components/propeties.jsx
@@ -3,7 +3,7 @@ import Property from './property-card';
 
 const Properties = () => (
   <Fragment>
-    <Property title="Test title" type="Test type" bedrooms="2" bathrooms="2" price="10000" city="Manchester" email="test@test.com" />
+    <Property title="Test title" type="Test type" bedrooms={2} bathrooms={2} price={10000} city="Manchester" email="test@test.com" />
   </Fragment>
 );
 
